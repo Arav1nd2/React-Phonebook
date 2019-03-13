d7dca390b0b051f770ebe6547fbd82db0354146d
Aravind Srinivasan <aravindsrinivasan2000@gmail.com>
2019-03-14 00:51:41 +0530

    Updates

---
+++ precache-manifest.a01b88f43eb843c00c427c2f9554d2ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ffd82262d59970576e4",
+    "url": "/React-Phonebook/static/js/runtime~main.5f3a81cc.js"
+  },
+  {
+    "revision": "4ba0259644ae3f610ca4",
+    "url": "/React-Phonebook/static/js/main.7792a39a.chunk.js"
+  },
+  {
+    "revision": "bff6ce8a78f29fdcb95d",
+    "url": "/React-Phonebook/static/js/2.32711bc3.chunk.js"
+  },
+  {
+    "revision": "4ba0259644ae3f610ca4",
+    "url": "/React-Phonebook/static/css/main.4ad3b8ef.chunk.css"
+  },
+  {
+    "revision": "52587407243f8ac31bffa8dc55ce4d6b",
+    "url": "/React-Phonebook/index.html"
+  }
+];
